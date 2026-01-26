e1248d2c2aa21ce0ae2a2b386f91ac9e2773b45f
jgiese <jan-philipp.giese@dataciders.com>
2026-01-26 17:48:41 +0100

    feat: refactor task integration tests for improved readability and functionality

--- cypress/e2e/integration/test-tasks.cy.ts
+++ cypress/e2e/integration/test-tasks.cy.ts
@@ -4,68 +4,56 @@ function navigateToTasks(): void {
     cy.get('nav.bottom-nav button:nth-child(2)').click();
 }
 
-function expandTask(selector: number): void {
-    cy.get(':nth-child(' + selector + ') > .task-page').click();
-}
 
 describe('Integration Tests - Tasks', (): void => {
 
     beforeEach((): void => {
         cy.loginTestUser();
         cy.reload();
+        navigateToTasks();
     });
 
     it('can navigate', (): void => {
-        navigateToTasks();
         cy.get('nav.bottom-nav button:nth-child(2)').should('have.class', 'is-active');
-        cy.get(':nth-child(1) > .task-page > .task-header > h3').should('contain', 'Photo Task');
+        cy.get('.tasks-container').should('be.visible');
     });
 
-    it('can expand a task', (): void => {
-        navigateToTasks();
-        expandTask(1);
-        cy.get(':nth-child(1) > .task-page .task-description').should('contain', 'Take a Photo');
+    it('can add, expand and remove tasks', (): void => {
+        addAndVerifyTasks();
+        expandVerifyAndRemoveTasks();
+        expandVerifyAndRemoveTasks();
     });
 
-    // TODO: Fix the upload functionality
-    it('can complete photo task', (): void => {
-        navigateToTasks();
-        cy.get('app-task-page app-file-input-button input').selectFile('test-data/test-upload.jpg', {force: true});
+    it('can complete and uncomplete task', (): void => {
+        cy.get('button.add-task-btn').click();
+        cy.get('app-task-page app-file-input-button input').selectFile('cypress/fixtures/test-upload.jpg', {force: true});
         cy.get('button.btn-primary').click();
         cy.get('app-task-page app-file-input-button').should('not.exist');
         cy.get('div.task-header').click();
         cy.get('.media-item > img').should('exist');
-
+        
         cy.get('path[d="M3 17.25V21h3.75l11.06-11.06-3.75-3.75L3 17.25zm14.71-9.04a1.003 1.003 0 0 0 0-1.42l-2.5-2.5a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.83-1.83z"]').click();
+        cy.get('button.btn-danger').click();
+        cy.get('div.confirm-dialog button.btn-danger').click();
+        cy.get('app-task-page app-file-input-button').should('exist');
+        
+        
+        cy.get('h3').click();
+        cy.get('button.delete-btn').click();
     });
 
-    it('can add a task', (): void => {
-        navigateToTasks();
+    function addAndVerifyTasks() {
         cy.get('button.add-task-btn').click();
-        cy.get(':nth-child(2) > .task-page > .task-header > h3').contains('Video Task');
+        cy.get('button.add-task-btn').click();
+
+        cy.get(':nth-child(1) > .task-page > .task-header').should('exist');
+        cy.get(':nth-child(2) > .task-page > .task-header').should('exist');
         cy.get('button.add-task-btn').should('not.exist');
-    });
+    }
 
-    it('can remove a task', (): void => {
-        navigateToTasks();
-        expandTask(2);
-        cy.get(':nth-child(2) > .task-page .delete-btn').click();
-    });
-
-    it('should display tasks container', (): void => {
-        navigateToTasks();
-        cy.get('app-tasks').should('be.visible');
-    });
-
-    it('should handle task interactions properly', (): void => {
-        navigateToTasks();
-
-        // Verify task elements exist
-        cy.get('.task-page').should('have.length.greaterThan', 0);
-
-        // Test task expansion/collapse
-        const taskSelector: number = 1;
-        expandTask(taskSelector);
-        cy.get(`:nth-child(${taskSelector}) > .task-page .task-content`).should('be.visible');
-    });
+    function expandVerifyAndRemoveTasks() {
+        cy.get('app-task-page:nth-child(1)').click();
+        cy.get(':nth-child(1) > .task-page > .task-content > .task-description').should('exist');
+        cy.get('button.delete-btn').click();
+    }
 });
