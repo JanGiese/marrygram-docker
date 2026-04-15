1c336b2ec7f64acbea661fc8d2aa70363b36e21d
jgiese <jan-philipp.giese@dataciders.com>
2026-04-15 18:36:29 +0200

    fix: update frontend and backend tags to latest in infra.test.sh

--- scripts/infra.test.sh
+++ scripts/infra.test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export FRONTEND_TAG=development
-export BACKEND_TAG=development
+export FRONTEND_TAG=latest
+export BACKEND_TAG=latest
 export MEDIA_COMPRESSOR_TAG=latest
 
 docker-compose -f compose.yml -f compose.test.yml "$@"
